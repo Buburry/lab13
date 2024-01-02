88c1b49a768174bd71590e26f3a5fd10cb558ae7
Buburry <Natapon7934@gmail.com>
2024-01-02 15:50:38 +0700

    lab13.1

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -17,3 +17,51 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[6]){
+
+    double sum,SD = 0.0,mean;
+    for(int i=0; i<N; i++) sum =sum+ A[i];
+    B[0] = sum/N;
+
+
+    mean = sum/N;
+    for (int i = 0; i < N; i++) {
+        SD += pow(A[i] - mean, 2);
+    }
+    SD /= (N); 
+    B[1] = sqrt(SD);    
+
+
+    double geo=1.0;
+    for (int i = 0; i < N; i++) {
+        geo = geo*A[i];
+    }
+    B[2] = pow(geo,1.0/N);
+
+
+    double sumInverse = 0.0;
+    for (int i = 0; i < N; ++i) {
+        sumInverse += 1.0 / A[i];
+    }
+    B[3] = N / sumInverse;
+
+    double maxVal = A[0];
+    for (int i = 1; i < N; ++i) {
+        if (A[i] > maxVal) {
+            maxVal = A[i]; 
+        }
+    }
+    B[4] =  maxVal;
+
+
+    double minVal = A[0];
+       for (int i = 1; i < N; ++i) {
+        if (A[i] < minVal) {
+            minVal = A[i];
+        }
+    }
+
+    B[5] =  minVal;
+}
+
